d5fcbf63814979e5b36cd853e24a69fe07d2536b
Coderyounes <younesanas2021@gmail.com>
2024-04-13 22:45:00 +0000

    Social Link Card Component

--- social-links-profile-main/tailwind.config.js
+++ social-links-profile-main/tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/*.html'],
+  content: ['./dist/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ['Inter'],
+      },
+      colors: {
+        'smooky-dark': '#233142',
+        grenny: '#97ED00',
+        grey: '#333333',
+        darkGey: '#1F1F1F',
+        offBlack: '#141414',
+      },
+    },
   },
   plugins: [],
 };
