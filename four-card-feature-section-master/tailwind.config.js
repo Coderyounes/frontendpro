cc79f9c894e2d22019247aee7c145326dac02fa2
Coderyounes <younesanas2021@gmail.com>
2024-04-15 18:42:39 +0100

    finish the mobile Version

--- four-card-feature-section-master/tailwind.config.js
+++ four-card-feature-section-master/tailwind.config.js
@@ -3,8 +3,16 @@ module.exports = {
   content: ['./dist/*.html'],
   theme: {
     extend: {
+      boxShadow: {
+        custom: 'rgba(0, 0, 0, 0.35) 0px 5px 15px',
+      },
       fontFamily: {
         poppins: ['Poppins'],
+      },
+      colors: {
+        lightgray: '#FAFAFA',
+        GrayishBlue: '#A3A5AE',
+        VeryDarkBlue: '#4C4E61',
       },
     },
   },
